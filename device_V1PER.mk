9f364b247c021f8d76d1409c982b5ae531d87d06
Gellert <hajasgellert@gmail.com>
2013-12-16 23:40:46 +0200

    6.0.3.7

---
+++ device_V1PER.mk
@@ -0,0 +1,24 @@
+$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+
+# The gps config appropriate for this device
+$(call inherit-product, device/common/gps/gps_us_supl.mk)
+
+$(call inherit-product-if-exists, vendor/ALLViEW/V1PER/V1PER-vendor.mk)
+
+DEVICE_PACKAGE_OVERLAYS += device/ALLViEW/V1PER/overlay
+
+LOCAL_PATH := device/ALLViEW/V1PER
+ifeq ($(TARGET_PREBUILT_KERNEL),)
+	LOCAL_KERNEL := $(LOCAL_PATH)/kernel
+else
+	LOCAL_KERNEL := $(TARGET_PREBUILT_KERNEL)
+endif
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_KERNEL):kernel
+
+$(call inherit-product, build/target/product/full.mk)
+
+PRODUCT_BUILD_PROP_OVERRIDES += BUILD_UTC_DATE=0
+PRODUCT_NAME := full_V1PER
+PRODUCT_DEVICE := V1PER
