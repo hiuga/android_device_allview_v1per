c2fa40e798366c17e87f821c478ed7e53831a3fb
Gellert <hajasgellert@gmail.com>
2013-12-17 02:02:45 +0200

    Initial Commit

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -3,14 +3,27 @@ USE_CAMERA_STUB := true
 # inherit from the proprietary version
 -include vendor/allview/v1per/BoardConfigVendor.mk
 
+TARGET_NO_BOOLOADER :=true
+TARGET_BOOTLOADER_BOARD_NAME := V1_ViPER
+
+# Platform
+TARGET_BOARD_PLATFORM := mtk6589
+TARGET_BOARD_PLATFORM_GPU := powervr-sgx544
+
+# Flags
+#TARGET_GLOBAL_CFLAGS += -mfpu=neon -mfloat-abi=softfp
+#TARGET_GLOBAL_CPPFLAGS += -mfpu=neon -mfloat-abi=softfp
+#COMMON_GLOBAL_CFLAGS += -DREFRESH_RATE=60 -DQCOM_HARDWARE
+
+# Architecture
 TARGET_ARCH := arm
-TARGET_NO_BOOTLOADER := true
+TARGET_CPU_VARIANT := cortex-a7
 TARGET_BOARD_PLATFORM := mt6589
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
 TARGET_ARCH_VARIANT := armv7-a-neon
 ARCH_ARM_HAVE_TLS_REGISTER := true
-
+#TARGET_CPU_SMP := true
 TARGET_BOOTLOADER_BOARD_NAME := V1_ViPER
 
 BOARD_KERNEL_CMDLINE := 
@@ -41,3 +54,17 @@ CWM_EMMC_RECOVERY_DEVICE_SIZE := 0x00600000
 CWM_EMMC_UBOOT_DEVICE_NAME := /dev/uboot
 CWM_EMMC_UBOOT_DEVICE_SIZE := 0x00060000
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/mt_usb/gadget/lun%d/file
+TARGET_USERIMAGES_USE_EXT4 := true
+#BOARD_HAS_LARGE_FILESYSTEM := true
+
+# TWRP Related Customization
+DEVICE_RESOLUTION := 720x1280
+#BOARD_HAS_NO_REAL_SDCARD := false
+#TW_NO_USB_STORAGE := false
+#RECOVERY_GRAPHICS_USE_LINELENGTH := true
+#TW_NO_REBOOT_BOOTLOADER := true
+#TW_BRIGHTNESS_PATH := /sys/devices/platform/leds-mt65xx/leds/lcd-backlight/brightness
+#TW_MAX_BRIGHTNESS := 255
+#TW_CUSTOM_BATTERY_PATH := /sys/devices/platform/mt6320-battery/power_supply/battery/
+
+BOARD_CUSTOM_BOOTIMG_MK := device/allview/v1per/custombootimg.mk
